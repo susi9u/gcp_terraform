976b40205b080d6b211fe806295570e9a3432011
Swetha Jayapal <127972473+RogithAnnamalai27@users.noreply.github.com>
2025-04-24 21:56:14 +0530

    initial code

---
+++ main.tf
@@ -0,0 +1,6 @@
+resource "google_storage_bucket" "my-bucket" {
+  name          = "no-public-access-bucket"
+  location      = "US"
+  force_destroy = true
+  public_access_prevention = "enforced"
+}
