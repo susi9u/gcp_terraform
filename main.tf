682638973b5ed43fc7ae86ae9e844e0a2dcd1eff
SuSi <susizme4@gmail.com>
2025-04-27 11:13:47 +0530

    Format

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "google_storage_bucket" "my-bucket" {
-  name          = "no-public-access-bucket"
-  location      = "US"
-  force_destroy = true
+  name                     = "no-public-access-bucket"
+  location                 = "US"
+  force_destroy            = true
   public_access_prevention = "enforced"
 }
